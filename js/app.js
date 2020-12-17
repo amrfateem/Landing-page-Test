f45ed0240ad0344a6a6dfbb64adde221b395e960
amrfateem <35631863+amrfateem@users.noreply.github.com>
2020-12-17 14:16:39 +0200

    Finishig up project requirements
    Adding Active state  and making some minor changes.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,34 @@
-//DOM to get tools variables for functions
+//DOM to get objects for functions
 let topb = document.getElementById("topbutton");
-window.onscroll = function () {
+let sections = document.querySelectorAll('.sectionbox');
+let navitems = document.querySelectorAll('.navitem');
+
+// global listen instead of addind addEventListener
+window.onscroll = () => {
 	scroll()
+	changenav()
 };
+
+// function to show which active state is in view and change style
+function changenav(){
+	sections.forEach((section) => {
+		sec=section.getBoundingClientRect();
+		if (sec.top <= 150 && sec.bottom > 200 ){
+			section.classList.add("activesec");		
+			section.classList.remove("notactivesec");
+		} else {
+			section.classList.remove("activesec");
+			section.classList.add("notactivesec");
+		} // to ensure smooth transiation betwwen active states
+	});
+}
 
 // function to show the button when goin down
 function scroll() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		topb.style.display = "block";
-	} else {
-		topb.style.display = "none";
-	}
+	document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? topb.style.display = "block" : topb.style.display = "none";
 }
 
 //function to scrollup
 function topFunction() {
 	document.documentElement.scrollTop = 0;
-} 
-
+}
